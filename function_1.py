8c8442cac453577c6cb0b4044460446b4868c19c
a.ignatov86 <a.ignatov86@gmail.com>
2023-04-29 17:50:09 +0300

    startCycles

--- function_1.py
+++ function_1.py
@@ -1,7 +1,17 @@
-def save_email():
-    email = '  SuppORT@hexlet.IO'
-    trim = email.strip()
-    prop = trim.lower()
-    return prop
+#entry = input('Password: ')
 
-print(save_email())
+#if entry == 'sub2b001':
+#    print('Access Granted')
+#else:
+#    print('Access Denied')
+
+#def f( a=1, b=2, c= None, d = 'nxy'):
+#    print(a,b,c,d)
+
+#print(f(7,d=1))
+
+def trim_and_repeat(string='python',offset=0,repetitions=1):
+    slise=string[offset:]
+    result = slise * repetitions
+    return result
+print(trim_and_repeat(offset=3, repetitions=2))
---
+++ function_2.py
@@ -0,0 +1,25 @@
+#def  is_a_start (text):
+#    start = text[0].lower()
+#    return start == 'a'
+#print(is_a_start('Atext'))
+
+##def is_correct_password(password):
+##    lenght = len(password)
+##
+ ##   return lenght > 8 and lenght < 20
+##print(is_correct_password('123456789'))
+
+#def is_not_even(num):
+#    return num % 2  != 0
+#print(not is_not_even(2))
+
+##def is_leap_year(num):
+##    a = (num % 400 ==0) or (num % 4 ==0 and num % 100 !=0)
+##    return a
+##print(is_leap_year(2016))
+
+def string_or_not(text):
+    is_string = isinstance(text, str)
+    answer = is_string and 'yes' or 'no'
+    return answer
+print (string_or_not(True))
